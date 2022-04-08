df903fc71f59b429fccc21aff2fbeab6c5552e37
31shal <102649832+31shal@users.noreply.github.com>
2022-04-08 16:26:39 +0530

    Create apb interface.sv

---
+++ apb interface.sv
@@ -0,0 +1,10 @@
+interface apb_intf(input clk,input reset);
+  logic paddr;
+  logic pwdata;
+  logic psel;
+  logic penable;
+  logic prdata;
+  logic prdata;
+  logic pready;
+  logic pwrite;
+endinterface
