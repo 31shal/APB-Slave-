d26422941d1cf25e468407f421f89fb14f12ca05
31shal <102649832+31shal@users.noreply.github.com>
2022-04-08 12:33:27 +0530

    Update apb_slave dut.sv

--- apb_slave dut.sv
+++ apb_slave dut.sv
@@ -7,8 +7,44 @@ input penable;
 input pwrite;
 input preset;
 input pclk;
-input[DATA_WIDTH-1:0] Pwdata;
+  input[DATA_WIDTH-1:0] pwdata;
 input [ADDR_width-1:0] Paddr;
 output reg pready;
 output reg prdata;
-l
+logic[DATA_WIDTH-1:0] mem[ADDR_WIDTH-1:0];
+  always@(posedge clk) begin
+    if(preset)begin
+      pready <=1;
+      psel<= 0;
+      penable<= 0;
+    for(i=0;i<ADDR_WIDTH;i++)
+      mem[i]=i;
+    end
+    case(state)begin
+      IDLE:begin
+        if(psel && !penable)
+        state<=SETUP;
+  else
+    state<=IDLE;
+      end
+      SETUP: begin
+        if(psel && penable)
+          state<= ACCESS;
+         else
+           state<= SETUP;
+      end
+      ACCESS: begin
+        if(!pready)
+          state<= ACCESS;
+        if(pready && pwrite)begin
+          mem[paddr] =pwdata;
+          state<=SETUP;
+        end
+        if(pready && !pwrite)begin
+          prdata =mem[paddr];
+        end
+      end
+      default: state= IDLE;
+      endcase
+      endmodule   
+          
